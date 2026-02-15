24c5c7c81bc0f0846ef2f799763b63918c7a3dae
jkmurphy123 <jkmurphy@zbzoom.net>
2026-02-15 07:57:58 -0500

    Added image quiz 2

---
+++ labyrinth/plugins/image_quiz_002/plugin.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from labyrinth.core.image_quiz import ImageQuizChallenge
+
+
+class Plugin(ImageQuizChallenge):
+    id = "image_quiz_002"
+    name = "Image Quiz 002"
